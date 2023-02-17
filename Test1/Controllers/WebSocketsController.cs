5b7d2970e0d86e849a6f62e59f7665bcfdcdea88
Dmitriy <123815654+Cabalette@users.noreply.github.com>
2023-02-17 15:04:24 +0300

    Добавьте файлы проекта.

---
+++ Test1/Controllers/WebSocketsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace Test1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WebSocketsController : ControllerBase
+    {
+        private readonly ILogger<WebSocketsController> _logger;
+
+        public WebSocketsController(ILogger<WebSocketsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/ws")]
+        public async Task Get()
+        {
+            if (HttpContext.WebSockets.IsWebSocketRequest)
+            {
+                using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+                _logger.Log(LogLevel.Information, "WebSocket connection established");
+                await Echo(webSocket);
+            }
+            else
+            {
+                HttpContext.Response.StatusCode = 400;
+            }
+        }
+
+        private async Task Echo(WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            int i = 0;
+            while (i<10)
+            {
+                var date = DateTime.Now.ToString();
+                var serverMsg = Encoding.UTF8.GetBytes($"Time is {date}. I = {i}");
+                await webSocket.SendAsync(new ArraySegment<byte>(serverMsg, 0, serverMsg.Length),
+                                          WebSocketMessageType.Text,
+                                          true,
+                                          CancellationToken.None);
+                await Task.Delay(2000);
+                i++;
+            } 
+            await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Just", CancellationToken.None);
+        }
+    }
+}
